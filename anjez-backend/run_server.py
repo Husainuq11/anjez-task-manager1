77fbe87d8262de1f34b0e54be937abd519c0c878
Anjez Developer <developer@anjez.com>
2025-07-30 10:13:21 +0300

    Edit run_server

--- anjez-backend/run_server.py
+++ anjez-backend/run_server.py
@@ -5,5 +5,5 @@ sys.path.insert(0, os.path.dirname(__file__))
 from src.main import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
-
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host=\'0.0.0.0\', port=port, debug=False) 
